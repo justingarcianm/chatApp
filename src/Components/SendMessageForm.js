e5a980b75fa23b67d453398f32fb48ba45db07c4
justingarcianm <justingarcianm@gmail.com>
2019-10-08 15:17:58 -0600

    add input to SendMessageForm

--- src/Components/SendMessageForm.js
+++ src/Components/SendMessageForm.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 export class SendMessageForm extends Component {
   render() {
-    return <div></div>;
+    return (
+      <form className="send-medssage-form">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          placeholder="fType your message and hit Enter"
+          type="text"
+        />
+      </form>
+    );
   }
 }
 
