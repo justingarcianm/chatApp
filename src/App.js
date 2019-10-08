ab71ff66a314541093f4b96f0c079af577aca2bb
justingarcianm <justingarcianm@gmail.com>
2019-10-08 13:47:20 -0600

    set up dummy data in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import Title from "./Components/Title";
 import MessageList from "./Components/MessageList";
 import SendMessageForm from "./Components/SendMessageForm";
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <MessageList />
-      <SendMessageForm />
-    </div>
-  );
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+];
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: DUMMY_DATA
+    };
+  }
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <MessageList messaeges={this.state.messages} />
+        <SendMessageForm />
+      </div>
+    );
+  }
 }
 
 export default App;
